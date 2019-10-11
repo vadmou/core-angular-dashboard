d3972708b7f9b7ab1a2ffc9e578fe34a6918e353
Vadym Borovok <vadym.borovok@gmail.com>
2019-10-11 09:11:45 +0300

    add table

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {id: 1, customer: {id: 1, name: 'Tom', email: 'test@test.com', state: 'US'}, total: 230, placed: new Date(2019, 12, 10), completed: new Date(2019, 12, 11)},
+    {id: 2, customer: {id: 1, name: 'Tom', email: 'test@test.com', state: 'US'}, total: 230, placed: new Date(2019, 12, 10), completed: new Date(2019, 12, 11)},
+    {id: 3, customer: {id: 1, name: 'Tom', email: 'test@test.com', state: 'US'}, total: 230, placed: new Date(2019, 12, 10), completed: new Date(2019, 12, 11)},
+    {id: 4, customer: {id: 1, name: 'Tom', email: 'test@test.com', state: 'US'}, total: 230, placed: new Date(2019, 12, 10), completed: new Date(2019, 12, 11)},
+    {id: 5, customer: {id: 1, name: 'Tom', email: 'test@test.com', state: 'US'}, total: 230, placed: new Date(2019, 12, 10), completed: new Date(2019, 12, 11)},
+    {id: 6, customer: {id: 1, name: 'Tom', email: 'test@test.com', state: 'US'}, total: 230, placed: new Date(2019, 12, 10), completed: new Date(2019, 12, 11)}
+  ];
 
   ngOnInit() {
   }
